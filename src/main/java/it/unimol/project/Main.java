443e6c2486a5d81df01607106114c4fcb0f0e5e2
simtape <s.cassetta1@studenti.unimol.it>
2021-05-21 17:00:37 +0200

    headless

--- src/main/java/it/unimol/project/Main.java
+++ src/main/java/it/unimol/project/Main.java
@@ -5,7 +5,7 @@ import it.unimol.project.gui.menu.MenuFrame;
 
 public class Main {
     public static void main(String args[]) {
-
+        System.setProperty("java.awt.headless", "true");
         MenuFrame.getInstance().createFrame();
     }
 }
